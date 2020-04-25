c8e2a89b97de605c926ede04653b7c304e9ee20d
Danish Ali Furniturewala <danishali7866@gmail.com>
2020-04-25 20:55:43 +0530

    Create game submit

--- routes/game.js
+++ routes/game.js
@@ -1,13 +1,31 @@
 var express = require('express');
 var router = express.Router();
 var roles = require(__dirname + '/../models/roles');
+var games = [];
 
 router.get('/', function(req, res, next) {
     res.render('create', { title: 'Create New Game', roles: roles });
 });
 
 router.post('/', function(req, res, next) {
-  res.render('game', { title: 'Game', roles: roles });
+  var game = {
+    name: req.body.username + '\'s Lobby',
+    roles: {
+      villagers: req.body.countOfVillager,
+      doctors: req.body.countOfDoctor,
+      cops: req.body.countOfCop,
+      mafia: req.body.countOfMafia,
+    },
+    players: [{
+      name: req.body.username,
+      owner: true,
+    }],
+    created: Date.now()
+  };
+  games.push(game);
+  res.render('game', { title: game.name, game: game });
 });
+
+
 
 module.exports = router;
