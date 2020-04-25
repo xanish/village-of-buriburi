91c450c7a30f079f3fca6fcff2b8c9d9a4f70466
Danish Ali Furniturewala <danishali7866@gmail.com>
2020-04-25 19:56:44 +0530

    Remodeled landing page

--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -1,8 +1,1 @@
-$(() => {
-    $('#join-game').on('click', () => {
-        $('#join-game-modal').css('visibility', 'visible');
-    });
-    $('.modal-close').on('click', () => {
-        $('#join-game-modal').css('visibility', 'hidden');
-    });
-});
+$(() => {});
